2ca3ebfa78711333bdbea07d07ec82b97bb6643b
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-13 17:56:08 +0530

    feat: create navbar & add basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <p>Flipkart</p>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header className="nav-container flex-centered-vertical">
+      <h1>Flipkart</h1>
+    </header>
+  );
+};
+
+export { NavBar };

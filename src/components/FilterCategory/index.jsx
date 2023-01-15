f70ad430ba099c738f75d7f138eb7e2ecdf1681f
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-15 22:35:34 +0530

    feat: add ability to collapse filter options in each filter category

--- src/components/FilterCategory/index.jsx
+++ src/components/FilterCategory/index.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import "./FilterCategory.css";
 
 const FilterCategory = ({ productFilter: { filterName, filterOptions } }) => {
+  const [isFilterOptionsCollapsed, setIsFilterOptionsCollapsed] =
+    useState(true);
+
   return (
-    <section className="filter-category-container">
+    <section
+      onClick={() => setIsFilterOptionsCollapsed((prev) => !prev)}
+      className="filter-category-container"
+    >
       <header className="filter-category-header">
         <h3 className="filter-category-header-heading">{filterName}</h3>
-        <i className="fa-solid fa-chevron-down"></i>
-        {/* <i className="fa-solid fa-chevron-up"></i> */}
+        <i
+          className={`fa-solid fa-chevron-${
+            isFilterOptionsCollapsed ? "down" : "up"
+          } filter-category-header-icon`}
+        ></i>
       </header>
-      <div className="filter-category-options">
-        {filterOptions.map((productFilterOption) => (
-          <label key={productFilterOption}>
-            <input type="checkbox" className="checkbox" />
-            {productFilterOption}
-          </label>
-        ))}
-      </div>
+      {!isFilterOptionsCollapsed && (
+        <div className="filter-category-options">
+          {filterOptions.map((productFilterOption) => (
+            <label key={productFilterOption}>
+              <input type="checkbox" className="checkbox" />
+              {productFilterOption}
+            </label>
+          ))}
+        </div>
+      )}
     </section>
   );
 };

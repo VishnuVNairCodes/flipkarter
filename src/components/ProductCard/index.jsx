cdaad834a58eb8596ac2e8a7bc4d0745b6a22bc9
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-15 11:14:42 +0530

    style: style product card, layout of ProductListMain

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,9 +1,47 @@
 import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  console.log(product);
+  const {
+    coverImg,
+    brand,
+    longDescription,
+    discountedPrice,
+    originalPrice,
+    discountPercent,
+    sizes,
+  } = product;
   return (
-    <article>
-      <h1> Product Card</h1>
+    <article className="product-card-container">
+      <div className="product-card-img-container">
+        <img className="product-card-img" src={coverImg} alt="product card" />
+      </div>
+      <div className="product-card-info-container">
+        <div className="product-card-info-header">
+          <h1 className="product-card-info-brand">{brand}</h1>
+          <div className="product-card-info-desc-container">
+            <p className="product-card-info-desc">{longDescription}</p>
+            <img
+              className="product-card-info-flipkart-assured"
+              src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_62673a.png"
+              alt="product card"
+            />
+          </div>
+        </div>
+        <div className="product-card-info-price-container">
+          <p className="discounted-price">₹{discountedPrice}</p>
+          <p className="original-price">
+            <del>₹{originalPrice}</del>
+          </p>
+          <p className="discount-percent">{discountPercent}% off</p>
+        </div>
+        <div className="product-card-sizes-container">
+          <p className="product-card-sizes-heading">Size</p>
+          {sizes.map((size) => (
+            <p className="product-card-sizes">{size}</p>
+          ))}
+        </div>
+      </div>
     </article>
   );
 };

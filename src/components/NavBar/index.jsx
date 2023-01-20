a5ca0e094f9aa9ed65967a8f27945424774f177b
Vishnu V Nair <vishnuvnair.ui@gmail.com>
2023-01-21 00:06:41 +0530

    change name to "Flipkarter"

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,7 +3,7 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <header className="nav-container flex-centered-vertical">
-      <h1>Flipkart</h1>
+      <h1>Flipkarter</h1>
     </header>
   );
 };
